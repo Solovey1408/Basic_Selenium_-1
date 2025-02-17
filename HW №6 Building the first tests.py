51d4f2d15d0690b528ac1c971de9092c80696037
Solovey1408 <songfromredstone@gmail.com>
2025-02-17 16:31:11 +0300

    ДЗ: Построение первых тестов

---
+++ HW №6 Building the first tests.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(
+    options=options,
+    service=ChromeService(ChromeDriverManager().install())
+)
+
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.set_window_size(1920, 1080)
+
+user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')
+user_name.send_keys("standard_user")
+print('Input login')
+
+user_password = driver.find_element(By.XPATH, '//input[@id="password"]')
+user_password.send_keys("secret_sauce")
+print('Input password')
+
+button_login = driver.find_element(By.ID, 'login-button')
+button_login.click()
+print('Click login button')
+
+get_url = driver.current_url
+url = 'https://www.saucedemo.com/inventory.html'
+assert url == get_url
+print('URL корректен')
+
+text_products = driver.find_element(By.XPATH, "//span[@class='title']")
+value_text_products = text_products.text
+assert value_text_products == "Products"
+print('Заголовок корректен')
