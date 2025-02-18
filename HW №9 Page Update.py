949b451dff3e3617de97e1308a670564507589f0
Solovey1408 <songfromredstone@gmail.com>
2025-02-18 20:40:30 +0300

    ДЗ: Обновление страницы

---
+++ HW №9 Page Update.py
@@ -0,0 +1,35 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+# options.add_argument("--headless") #Браузер открывается в headless режиме
+
+driver = webdriver.Chrome(
+    options=options,
+    service=ChromeService(ChromeDriverManager().install())
+)
+
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.set_window_size(1920, 1080)
+
+user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')
+user_name.send_keys('standard_use')
+print('Input Login')
+
+password = driver.find_element(By.XPATH, '//input[@id ="password"]')
+password.send_keys('secret_sauce')
+print('Input Password')
+
+button_login = driver.find_element(By.ID, 'login-button')
+button_login.click()
+print('Click login button')
+
+time.sleep(3)
+driver.refresh()
