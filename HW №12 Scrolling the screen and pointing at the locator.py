08ee35e787170f7fd3527d443279d4bb9b7a0171
Solovey1408 <songfromredstone@gmail.com>
2025-02-20 11:16:37 +0300

    ДЗ: Скроллинг экрана и наведение по локатору

--- HW №12 Scrolling the screen and pointing at the locator.py
+++ HW №12 Scrolling the screen and pointing at the locator.py
@@ -1,7 +1,3 @@
-import time
-import datetime
-import os #Импорт модуля os для работы с файлами в системе
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -34,20 +30,13 @@ button_login.click()
 print('Click login button')
 
 #Добавляем товары в корзину
-button_add_sauce_labs_backpack = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")
-button_add_sauce_labs_backpack.click()
-button_add_sauce_labs_bike_light = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-bike-light']")
-button_add_sauce_labs_bike_light.click()
-button_add_sauce_labs_bolt_t_shirt = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']")
-button_add_sauce_labs_bolt_t_shirt.click()
-button_add_sauce_labs_fleece_jacket = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-fleece-jacket']")
-button_add_sauce_labs_fleece_jacket.click()
-button_add_sauce_labs_onesie = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-onesie']")
-button_add_sauce_labs_onesie.click()
-button_add_test_all_the_things_t_shirt_red = driver.find_element(By.XPATH, "//button[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")
-button_add_test_all_the_things_t_shirt_red.click()
-button_shopping_cart_link = driver.find_element(By.XPATH, "//a[@data-test='shopping-cart-link']")
-button_shopping_cart_link.click() #Открываем корзину
+button_add_sauce_labs_backpack = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']").click()
+button_add_sauce_labs_bike_light = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-bike-light']").click()
+button_add_sauce_labs_bolt_t_shirt = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']").click()
+button_add_sauce_labs_fleece_jacket = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-fleece-jacket']").click()
+button_add_sauce_labs_onesie = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-onesie']").click()
+button_add_test_all_the_things_t_shirt_red = driver.find_element(By.XPATH, "//button[@id='add-to-cart-test.allthethings()-t-shirt-(red)']").click()
+button_shopping_cart_link = driver.find_element(By.XPATH, "//a[@data-test='shopping-cart-link']").click()
 
 
 actions = ActionChains(driver)
